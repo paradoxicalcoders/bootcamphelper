1d1edcae6b0204ff6528efd30e78841ae1e750f8
nomad <sbefort@gmail.com>
2019-06-24 15:25:34 -0500

    Setup React Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import LoginPage from './pages/LoginPage';
+
+const router = (
+  <Router>
+    <div>
+      <Route path="/" component={LoginPage} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
 registerServiceWorker();
