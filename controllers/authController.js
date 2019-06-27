8c8d3034a2770b6dce6611db2662463e7426e8e2
Byron Ferguson <ferguson.bs@gmail.com>
2019-06-26 22:19:16 -0500

    //WIP adding new models, issues with N:M create

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,49 @@
 const db = require('../models');
 
-// Inspect user to see if they should be considered an admin
-const updateIsAdmin = async (req, _res, next) => {
-  const STUDENT_ROLE_ID = 2;
-  // Filter all enrollments based on student role, if any non-student role found assume admin
-  const isAdmin = req.user.enrollments.filter(enrollment => enrollment.courseRole.id !== STUDENT_ROLE_ID).length !== 0;
+const STUDENT_ROLE_ID = 2;
 
-  req.user.userAccount.isAdmin = isAdmin;
-  next();
+// Filter all enrollments based on student role, if any non-student role found assume admin
+const isAdmin = user => user.enrollments.filter(enrollment => enrollment.courseRole.id !== STUDENT_ROLE_ID).length !== 0;
+
+const fetchUser = async (req, res, next) => {
+  const { userAccount: account, enrollments } = req.user;
+  const user = {
+    id: account.id,
+    firstName: account.firstName,
+    lastName: account.lastName,
+    email: account.email,
+    githubUsername: account.githubUserName,
+    isAdmin: isAdmin(req.user),
+    authToken: account.authToken,
+    enrollments: enrollments.map(enrollment => ({
+      id: enrollment.id,
+      courseId: enrollment.course.id,
+      cohortId: enrollment.course.cohortId,
+      startDate: enrollment.course.startDate,
+      endDate: enrollment.course.endDate,
+    })),
+  };
+
+  try {
+    await db.User.create(user, {
+      include: [db.Enrollment],
+    });
+
+    const rs = await db.User.findOne({
+      where: { id: req.user.userAccount.id },
+      include: [db.Enrollment],
+    });
+
+    console.log(JSON.stringify(rs, null, 2));
+
+    next();
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 // UPSERT user into local DB (ensures updated authToken)
-const updateUser = async (req, _res, next) => {
+const updateUser = async (req, res, next) => {
   // upsert user
   const { userAccount: account } = req.user;
   const user = {
@@ -24,22 +56,50 @@ const updateUser = async (req, _res, next) => {
     authToken: account.authToken,
   };
 
-  req.user.userAccount = await db.User.upsert(user, { returning: true });
+  try {
+    await db.User.upsert(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 
   next();
 };
 
 // Compare user enrollments against the local DB
 const updateEnrollments = async (req, res) => {
-  const { enrollments } = req.user;
-  // query DB for enrollment count
-  // compare DB record count against enrollments.length
-  // if matched return user object
-  console.log('Enrollment Count', enrollments.length);
-  // else upsert enrollments into DB
+  const enrollments = req.user.enrollments.map(enrollment => ({
+    id: enrollment.id,
+    courseId: enrollment.courseId,
+    cohortId: enrollment.course.cohortId,
+    startDate: enrollment.course.startDate,
+    endDate: enrollment.course.endDate,
+  }));
 
-  res.json(req.user);
+  try {
+    // query DB for enrollment count
+    const rs = await db.Enrollment.findAll({
+      include: [{
+        model: db.User,
+        where: {
+          id: req.user.userAccount.id,
+        },
+      }],
+    });
+
+    // compare DB record count against enrollments.length
+    if (enrollments.length === rs.length) {
+      // if matched return user object
+      res.json(req.user);
+    } else {
+      // else upsert enrollments into DB
+      const enrollmentUpserts = enrollments.map(enrollment => db.Enrollment.upsert(enrollment));
+      await Promise.all(enrollmentUpserts);
+      res.json(req.user);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 
-exports.login = [updateIsAdmin, updateUser, updateEnrollments];
+exports.login = [fetchUser, updateUser, updateEnrollments];
---
+++ models/enrollment.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const Enrollment = sequelize.define('Enrollment', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: false,
+    },
+    courseId: DataTypes.INTEGER,
+    cohortId: DataTypes.INTEGER,
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE,
+  }, {});
+
+  Enrollment.associate = (models) => {
+    models.Enrollment.hasMany(models.User);
+    models.Enrollment.hasMany(models.Question);
+    models.Enrollment.belongsToMany(models.Question, { through: 'enrollment_questions' });
+    models.Enrollment.belongsToMany(models.User, { through: 'user_enrollments' });
+  };
+
+  return Enrollment;
+};
