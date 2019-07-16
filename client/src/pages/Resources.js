2449a20c82984b08eed1ac9ac05981b002ebc608
Shaun Befort <sbefort@gmail.com>
2019-07-16 08:03:36 -0500

    Added a React select box for tags

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Paper from '@material-ui/core/Paper';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
+import isUrl from 'validator/lib/isURL';
+import Select from 'react-select';
 
 import AddIcon from '@material-ui/icons/Add';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -14,6 +17,31 @@ import TextField from '@material-ui/core/TextField';
 import TitleIcon from '@material-ui/icons/Title';
 
 import ContentWrapper from 'components/ContentWrapper';
+import Snackbar from 'components/Snackbar';
+
+const suggestions = [
+  { label: 'JavaScript' },
+  { label: 'SQL' },
+].map(suggestion => ({
+  value: suggestion.label,
+  label: suggestion.label,
+}));
+
+const customSelectStyles = {
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = 'opacity 300ms';
+
+    return { ...provided, opacity, transition };
+  },
+  control: provided => ({
+    ...provided,
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: '1rem',
+    paddingLeft: 4,
+  }),
+};
 
 class Resources extends Component {
   constructor(props) {
@@ -22,10 +50,14 @@ class Resources extends Component {
       title: '',
       url: '',
       tabValue: 2,
+      snackbarVariant: 'warning',
+      snackbarMessage: '',
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleTabChange = this.handleTabChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
 
   render() {
@@ -50,6 +82,12 @@ class Resources extends Component {
             {this.renderTabContent()}
           </Box>
         </Paper>
+        <Snackbar
+          open={!!this.state.snackbarMessage}
+          onClose={this.closeSnackbar}
+          message={this.state.snackbarMessage}
+          variant={this.state.snackbarVariant}
+        />
       </ContentWrapper>
     );
   }
@@ -68,7 +106,11 @@ class Resources extends Component {
       >
         <form
           onSubmit={this.handleSubmit}
-          style={{width: '100%', maxWidth: 600, paddingLeft: 10, paddingRight: 10 }}
+          style={
+            {
+              width: '100%', maxWidth: 600, paddingLeft: 10, paddingRight: 10,
+            }
+          }
         >
           <Box my={2}>
             <TextField
@@ -108,6 +150,24 @@ class Resources extends Component {
               }}
             />
           </Box>
+          <Box my={2}>
+            <Select
+              id="tags"
+              options={suggestions}
+              placeholder="Tags"
+              // value={multi}
+              // onChange={handleChangeMulti}
+              isMulti
+              styles={customSelectStyles}
+              theme={theme => ({
+                ...theme,
+                colors: {
+                  ...theme.colors,
+                  primary: '#3f51b5',
+                },
+              })}
+            />
+          </Box>
           <Box mt={1}>
             <Button
               type="submit"
@@ -131,6 +191,53 @@ class Resources extends Component {
 
   handleInputChange(event) {
     this.setState({ [event.target.name]: event.target.value });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+
+    if (!isUrl(this.state.url)) {
+      this.setState({
+        snackbarMessage: 'Invalid URL',
+        snackbarVariant: 'error',
+      });
+      return;
+    }
+
+    this.setState({
+      isLoading: true,
+    });
+
+    try {
+      const response = await axios.post('/api/v1/resources', {
+        url: this.state.url,
+        title: this.state.title,
+      });
+
+      if (response.data) {
+        const { tags } = this.state;
+        tags.push(response.data);
+        this.setState({
+          isLoading: false,
+          tags,
+          snackbarMessage: 'Tag added!',
+          snackbarVariant: 'success',
+          newTagName: '',
+        });
+      }
+    } catch (err) {
+      this.setState({
+        isLoading: false,
+        snackbarMessage: err.toString(),
+        snackbarVariant: 'error',
+      });
+    }
+  }
+
+  closeSnackbar() {
+    this.setState({
+      snackbarMessage: '',
+    });
   }
 }
 
